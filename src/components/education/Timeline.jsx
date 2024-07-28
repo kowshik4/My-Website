4165962aa53bd6ae0fdd040aded0d1f31b0afa3e
kowshik4 <kowshik.sai98@gmail.com>
2024-07-28 14:54:07 -0400

    Timeline

--- src/components/education/Timeline.jsx
+++ src/components/education/Timeline.jsx
@@ -5,19 +5,20 @@ import './Timeline.css';
 const Timeline = ({ experiences }) => {
   return (
     <section className='timeline section' id='timeline'>
-    <div className="timeline">
-      {experiences.map((exp, index) => (
-        <div key={index} className="timeline-item">
-          <div className="timeline-dot"></div>
-          <div className="timeline-date">{exp.duration}</div>
-          <div className="timeline-content">
-            <h3>{exp.role}</h3>
-            <h4>{exp.company}</h4>
-            <p>{exp.description}</p>
+      <div className="timeline__container container grid">
+      <div className="timeline-line"></div>
+        {experiences.map((exp, index) => (
+          <div key={index} className={`timeline-item ${index % 2 === 0 ? 'left' : 'right'}`}>
+            <div className="timeline-dot"></div>
+            <div className={`timeline-date ${index % 2 === 0 ? 'right' : 'left'}`}>{exp.duration}</div>
+            <div className="timeline-content">
+              <h3>{exp.role}</h3>
+              <h4>{exp.company}</h4>
+              <p>{exp.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </section>
   );
 };
